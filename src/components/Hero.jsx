617366bc8ce9c71035e20b1d8459ac090cfa25a7
rimple-chaudhary <rimplechaudhary1@gmail.com>
2025-03-09 19:09:19 +0530

    added favicon,changed resume download link

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -131,7 +131,7 @@ export default function Hero() {
 								></a>
 
 								<a
-									href="public\files\resume.pdf"
+									href="/files/resume.pdf"
 									download="Rimple_Resume.pdf"
 									className="group relative inline-flex items-center justify-center gap-3 p-0.5 rounded-xl bg-gradient-to-r from-gray-800 to-gray-700 transition-all duration-300 hover:scale-105 hover:shadow-[0_0_2rem_-0.5rem_#60A5FA]"
 								>
